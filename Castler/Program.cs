028035a974964587a90887da0c98870f0fbf5dd2
Guilherme Simão Costa <gui_sc@outlook.com>
2021-12-22 02:50:47 -0300

    Adicionar arquivos de projeto.

---
+++ Castler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Castler
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game())
+                game.Run();
+        }
+    }
+}
